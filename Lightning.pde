c70d163dd10e35fe0f25256fb9bd7a1de408447d
celenac <changcelena@gmail.com>
2014-09-04 10:31:04 -0700

    first commit

--- Lightning.pde
+++ Lightning.pde
@@ -1,13 +1,33 @@
+int startX=0;
+  int startY=150;
+  int endX=0;
+  int endY=150;
+
 void setup()
 {
-  size(300,300);
+  size(500,400);
+  frameRate(500);
+  background(0);
+  startX=250;
+	startY=0;
 }
 void draw()
 {
-
+	stroke(255, (int)(Math.random()*4)+232, (int)(Math.random()*255));
+  strokeWeight((int)(Math.random()*3));
+	endX=startX+((int)(Math.random()*10)+(int)(Math.random()*-10));
+	endY=startY+((int)(Math.random()*-10)+10);
+	line(startX, startY,endX,endY);
+	startX=endX;
+	startY=endY;
 }
+
 void mousePressed()
 {
 
+startX=(int)(Math.random()*500);
+ startY=0;
+ endX=0;
+endY=150;
 }
 
--- build-tmp/source/Lightning.java
+++ build-tmp/source/Lightning.java
@@ -14,19 +14,37 @@ import java.io.IOException;
 
 public class Lightning extends PApplet {
 
+int startX=0;
+  int startY=150;
+  int endX=0;
+  int endY=150;
+
 public void setup()
 {
-  size(300,300);
-  noLoop();
+  size(500,400);
+  frameRate(500);
+  background(0);
+  startX=250;
+	startY=0;
 }
 public void draw()
 {
-  int x = (int)(Math.random() * 300);
-  ellipse(x,150,10,10);
+	stroke(255, (int)(Math.random()*4)+232, (int)(Math.random()*255));
+  strokeWeight((int)(Math.random()*3));
+	endX=startX+((int)(Math.random()*10)+(int)(Math.random()*-10));
+	endY=startY+((int)(Math.random()*-10)+10);
+	line(startX, startY,endX,endY);
+	startX=endX;
+	startY=endY;
 }
+
 public void mousePressed()
 {
-  redraw();
+
+startX=(int)(Math.random()*500);
+ startY=0;
+ endX=0;
+endY=150;
 }
 
   static public void main(String[] passedArgs) {
